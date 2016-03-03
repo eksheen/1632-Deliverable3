ec6e7534f7cfd4ddd8b03464b3e19a67489dac3f
Neel Kowdley <nkowdley@gmail.com>
2016-03-02 21:58:44 -0500

    remove test

--- src/Deliverable3/HackerNewsTester.java
+++ src/Deliverable3/HackerNewsTester.java
@@ -40,22 +40,6 @@ public class HackerNewsTester {
 		assertTrue(title.contains("Hacker News"));
 		
 	}
-	
-	//Given that I am on the login page
-   	//When I dont have an account
-   	//Then I can't create one with a blank username
-   	@Test
-   	public void testCreateAccountBlankUsername() {
-   		try {
-   			driver.findElement(By.name("username:")).sendKeys("");
-   		} catch (NoSuchElementException nseex) {
-			fail();
-		}
-		WebElement loginDiv = driver.findElement(By.id("Submit"));
-   		
-		WebElement submitButton = loginDiv.findElement(By.className("Submit"));
-		submitButton.click();
-   	}
    	
 	  @Test
 	  public void testLoginNoUsernameNoPassword() throws Exception {
