388d29249c372cd34599812fd9f1c0a846244ee8
Neel Kowdley <nkowdley@gmail.com>
2016-03-02 21:59:15 -0500

    close firefox

--- src/Deliverable3/HackerNewsTester.java
+++ src/Deliverable3/HackerNewsTester.java
@@ -117,7 +117,7 @@ public class HackerNewsTester {
 	  	  
 	  @After
 	  public void tearDown() throws Exception {
-	    //driver.quit();
+	    driver.quit();
 	    String verificationErrorString = verificationErrors.toString();
 	    if (!"".equals(verificationErrorString)) {
 	      fail(verificationErrorString);
